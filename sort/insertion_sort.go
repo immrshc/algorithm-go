d703f3b1ba2cc437ddb19c77a153919539387e98
shoichiimamura <imamura@c-fo.com>
2020-06-21 17:29:51 +0900

    :sparkles: Implement insertion sort

--- sort/insertion_sort.go
+++ sort/insertion_sort.go
@@ -1,1 +1,16 @@
 package sort
+
+func InsertionSort(items []int) {
+	n := len(items)
+	// 何番目までソートするか
+	for i := 0; i < n-1; i++ {
+		j := i
+		// 指定の範囲内がソートされるまで入れ替える
+		for j >= 0 && items[j] > items[j+1] {
+			tmp := items[j]
+			items[j] = items[j+1]
+			items[j+1] = tmp
+			j--
+		}
+	}
+}
--- sort/sort.go
+++ sort/sort.go
@@ -5,6 +5,7 @@ import "fmt"
 func PrintResult(items []int) {
 	fmt.Printf("unsorted: %v\n", items)
 	// BubbleSort(items)
-	SelectionSort(items)
+	// SelectionSort(items)
+	InsertionSort(items)
 	fmt.Printf("sorted:   %v\n", items)
 }
